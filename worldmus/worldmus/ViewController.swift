ba57b921b4866efc1b612b157696f23b1c4b60ce
wangsupershuang <wangsupershuang@126.com>
2016-09-27 16:33:48 +0800

    Add files via upload

---
+++ worldmus/worldmus/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  worldmus
+//
+//  Created by wwll on 16/9/27.
+//  Copyright © 2016年 wangshuang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
